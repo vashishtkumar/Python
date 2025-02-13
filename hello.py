88f0be463d05202590a1ef46814c539efa274362
vashishtkumar <vashishtk270@gmail.com>
2025-02-13 18:07:22 +0530

    basic

--- hello.py
+++ hello.py
@@ -1,1 +1,91 @@
 print("Hello Vashisht")
+
+print(45)
+
+print(" the sum of a+b is",45+65)
+
+name="vashisht"
+
+age=19
+
+cgpa=9.44
+
+print(name,age,cgpa)
+
+print("my name is: ",name,"my age is: ",age)
+
+print(type(name))
+print(type(cgpa))
+print(type(age))  
+
+a=None
+print(type(a))
+
+
+# there are four types of datatypes
+"""
+Integer
+String
+NoneType
+boolean
+float
+"""
+
+
+#Types of operator
+
+"""
+Arithematic=(*,-,/,%,**);
+Relational Operator=(==,!=,>=,<=)
+Assisgnement Operator=(=,!=,+=,*=,**=)
+Logical(not,and,or)
+
+** power opeator
+
+"""
+
+
+num1=2
+num2=3
+
+print(num1**num2)
+
+# logical operator
+
+print(not (num1>num2))
+
+print(0 or 9)
+
+val1=True
+val2=True
+
+print(val1 and val2)
+print(val1==val2)
+
+print(val1!=val2)
+
+#type conversion
+
+a=9
+b=9.9
+
+print(a+b)
+
+# type casting
+
+a=int("2")
+b=9
+print(a+b)
+
+
+b=str(9.9)
+
+print(b)
+print(type(b))
+
+# input   it is always string
+name =input("enter you name")
+
+print("welcome ",name)
+
+val=int(input("enter your number"))
