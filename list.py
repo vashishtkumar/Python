29fbda25f0baac5d09a1756ce0e55fd06f5b1063
vashishtkumar <vashishtk270@gmail.com>
2025-05-08 20:36:43 +0530

    complete list

--- list.py
+++ list.py
@@ -6,6 +6,39 @@
 marks=[87, 64, 33, 95, 76]
 
 student=["vashist", 25, "Male"]
-print()
+print(student)
 student[0]="Vashisht Kumar"
+print(student)
 
+print(len(student))  # return the size of typle
+
+# Slicing
+print(student[1:3])
+print(student[-3:-1])
+print(student[-1:-3:-1])
+print(student)
+
+#Some List Methods
+
+list =[2,1,3]
+
+list.append(4)
+print(list)
+
+list.sort() #To sort the list in ascending order
+print(list)
+
+list.sort(reverse=True) # To sort the lsit in descending order
+print(list)
+
+list.reverse()  #To revese the list
+print(list)
+
+list.insert(1,5) # To insert data idx,ele
+print(list)
+
+list.remove(5)  # Remove the first occurence of of that particilar element
+print(list)
+
+list.pop(0) # Removes the particular index
+print(list)
