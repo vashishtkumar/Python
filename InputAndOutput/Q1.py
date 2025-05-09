cef988e96e8f9440bd5676122e779e458d87b25d
vashishtkumar <vashishtk270@gmail.com>
2025-05-09 20:42:33 +0530

    half task done

---
+++ InputAndOutput/Q1.py
@@ -0,0 +1,11 @@
+"""Create a new file “practice.txt” using python. Add the following data in it:
+Hi everyone
+we are learning File I/O
+using Java.
+I like programming in java
+"""
+
+f=open("practice.txt","w")
+
+f.write("Hi everyone \n we are learning File I/O \n using Java.\nI like programming in java")
+f.close()
