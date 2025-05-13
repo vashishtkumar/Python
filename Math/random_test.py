2c6763dc82f8db3857ab1fcd9e3a2a6cd31790cd
vashishtkumar <vashishtk270@gmail.com>
2025-05-13 18:26:58 +0530

    random module

---
+++ Math/random_test.py
@@ -0,0 +1,14 @@
+import random as rd
+
+# so rd.random()
+# rd.randint()
+#rd.choice()
+#rd.randrange()
+
+print(rd.random())
+print(rd.randint(1,19))
+
+li=("Vashisht",90,"kumar",False)
+
+print(rd.choice(li))
+print(rd.randrange(10,4,-2))
