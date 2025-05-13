9541ee1289848aa40633753ae91eed3ab9ae97f9
vashishtkumar <vashishtk270@gmail.com>
2025-05-13 18:05:25 +0530

    math module

---
+++ Math/math.py
@@ -0,0 +1,15 @@
+# import math
+
+# print(math.sqrt(5))
+
+# print(math.pow(5,5))
+
+# print(math.ceil(5.9))
+
+# print(math.floor(7.8))
+
+# print(math.factorial(5))
+
+from math import sqrt,pow
+
+print(sqrt(5))
